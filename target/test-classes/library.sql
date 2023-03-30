842305037f105a73b207184533c2d87993a20330
helay <mustafa.helay@gmail.com>
2023-03-30 13:53:44 -0400

    US 03 Helay

---
+++ target/test-classes/library.sql
@@ -0,0 +1,22 @@
+-- US 01
+select count(id) from users; --
+-- 1855
+
+select count(distinct id) from users;
+-- 1855
+
+
+-- RESULT --> MANUALLY IT IS PASSED
+
+
+-- US 02
+select * from users;
+
+
+
+-- US 02
+select count(*) from book_borrow
+where is_returned=0;
+
+-- US 03
+select name from book_categories;
